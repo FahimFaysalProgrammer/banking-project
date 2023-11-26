5e884afb1930964932b783ba4fc0410b3280782e
FahimFaysalProgrammer <fahim.faysal.programmer@gmail.com>
2023-11-26 20:02:18 +0600

    Making dynamic deposit system in my site using JavaScript

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
 // Check JavaScript file link:
 // alert("Hello World!");
+
+// Deposit Section:
+function handleDeposit() {
+  // console.log("Hello World!");
+  var inputValue = document.getElementById("deposit-input").value;
+  //console.log(inputValue);
+  var convertedInputValue = convertStringToNumber(inputValue);
+
+  var depositAmount = document.getElementById("deposit-amount").innerText;
+  // console.log(depositAmount);
+  var convertedDepositAmount = convertStringToNumber(depositAmount);
+
+  // console.log(typeof convertedInputValue, typeof convertedDepositAmount);
+
+  var sum = convertedInputValue + convertedDepositAmount;
+  // console.log(sum);
+  document.getElementById("deposit-amount").innerText = sum;
+  document.getElementById("deposit-input").value = "";
+}
+
+function convertStringToNumber(value) {
+  return parseFloat(value);
+}
